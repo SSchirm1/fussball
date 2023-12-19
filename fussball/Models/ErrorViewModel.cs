4e6032988518c9985c98adc57e69f045947117db
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2023-12-19 15:22:13 +0100

    Add project files.

---
+++ fussball/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fussball.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
