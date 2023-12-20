90a88fc7b7ba0f3910ec93cbc55dea7e5ef54df4
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2023-12-20 12:14:58 +0100

    .

--- fussball/Models/Kamp.cs
+++ fussball/Models/Kamp.cs
@@ -7,7 +7,7 @@ namespace fussball.Models
     {
         public int Id { get; set; }
         public DateTime Dato { get; set; }
-        public List<Spiller> Lag1 { get; set; }
-        public List<Spiller> Lag2 { get; set; }
+        public List<Spiller>? Lag1 { get; set; }
+        public List<Spiller>? Lag2 { get; set; }
     }
 }
